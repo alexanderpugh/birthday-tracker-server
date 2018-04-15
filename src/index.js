a8d8a92b947b766102b35e2977cebb6c003c3bb4
Alexander Pugh <alexanderpugh1991@gmail.com>
2018-04-15 12:00:51 +0100

    edit port env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('./config/db');
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, (err) => {
   if (!err) {
